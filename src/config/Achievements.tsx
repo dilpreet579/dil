cdbd6620b06d2f071b0a38e720de8b2cd8526f38
Dilpreet Singh <dilpreet082023@gmail.com>
2026-03-03 09:00:30 +0530

    feat: add acheivements

--- src/config/Achievements.tsx
+++ src/config/Achievements.tsx
@@ -1,9 +1,15 @@
 export const certificates = [
   {
-    file: '/assets/mlsummerschool.png',
-    title: 'Amazon ML Summer School 2025 - Selected Participant',
+    file: '/assets/mlsummerschool.jpg',
+    title: 'Amazon ML Summer School 2025 Completion Certificate',
     issuer: 'Amazon',
-    date: '2025-08-01',
+    date: '2025-09-10',
+  },
+  {
+    file: '/assets/sparkathon.jpg',
+    title: 'Walmart Sparkathon 2025 Participation Certificate',
+    issuer: 'Walmart',
+    date: '2025-08-07',
   },
 ];
 
